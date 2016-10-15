6181e545b587ba7368e10ddb1974dc01063d3b5b
Timothy Yudi <timothyyudi@gmail.com>
2016-10-15 13:25:36 +0700

    final

--- app/src/main/java/timothyyudi/trycamera/MainActivity.java
+++ app/src/main/java/timothyyudi/trycamera/MainActivity.java
@@ -2,20 +2,28 @@ package timothyyudi.trycamera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import pub.devrel.easypermissions.EasyPermissions;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1001; //bisa menggunakan angka berapapun
+    static final int REQUEST_TAKE_PHOTO = 1001; //use any number
 
-    Button btnImgCamera, btnImgStorage;
+    Button btnImgCamera;
     ImageView ivImage;
 
     @Override
@@ -25,8 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnImgCamera = (Button) findViewById(R.id.btnImgCamera);
         btnImgCamera.setOnClickListener(this);
-        btnImgStorage = (Button) findViewById(R.id.btnImgStorage);
-        btnImgStorage.setOnClickListener(this);
 
         ivImage = (ImageView) findViewById(R.id.ivImage);
     }
@@ -35,22 +41,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         if(view.getId()==R.id.btnImgCamera){
             dispatchTakePictureIntent();
-        }else if(view.getId()==R.id.btnImgStorage){}
+        }
     }
 
     private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//        }
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.v("TryCamera",ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ivImage.setImageBitmap(imageBitmap);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            ivImage.setImageBitmap(imageBitmap);
+//            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+
+            //load image from path
+            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, null);
+            ivImage.setImageBitmap(bitmap);
         }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
